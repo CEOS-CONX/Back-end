13c97a80472db9da80fba65129570103906ad60d
김도현 <kimdoes2143@naver.com>
2026-05-04 18:00:32 +0900

    fisrt commit

---
+++ src/main/java/com/conx/server/ConxServerApplication.java
@@ -0,0 +1,13 @@
+package com.conx.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConxServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConxServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conx/server/ConxServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conx.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConxServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
