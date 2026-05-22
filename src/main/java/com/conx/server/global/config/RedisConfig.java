3ffe96ed442dda7f3f18c27ce854564b808c529c
김도현 <kimdoes2143@naver.com>
2026-05-23 05:08:04 +0900

    feat: 인증인가 작업 완료

--- src/main/java/com/conx/server/global/config/RedisConfig.java
+++ src/main/java/com/conx/server/global/config/RedisConfig.java
@@ -1,66 +1,83 @@
 package com.conx.server.global.config;
 
-import org.springframework.boot.cache.autoconfigure.RedisCacheManagerBuilderCustomizer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import tools.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 
 import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
 
-        GenericJacksonJsonRedisSerializer serializer =
-                GenericJacksonJsonRedisSerializer.builder()
-                        .enableDefaultTyping(
-                                BasicPolymorphicTypeValidator.builder()
-                                        .allowIfSubType(Object.class)
-                                        .build()
-                        )
-                        .build();
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(
+            RedisConnectionFactory connectionFactory
+    ) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.activateDefaultTyping(
+                BasicPolymorphicTypeValidator.builder()
+                        .allowIfSubType(Object.class)
+                        .build(),
+                ObjectMapper.DefaultTyping.NON_FINAL
+        );
+
+        GenericJackson2JsonRedisSerializer serializer =
+                new GenericJackson2JsonRedisSerializer(objectMapper);
+
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+
+        template.setConnectionFactory(connectionFactory);
 
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(serializer);
+
         template.setHashKeySerializer(new StringRedisSerializer());
         template.setHashValueSerializer(serializer);
+
         template.afterPropertiesSet();
 
         return template;
     }
 
-
     @Bean
     public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
-        GenericJacksonJsonRedisSerializer serializer =
-                GenericJacksonJsonRedisSerializer.builder()
-                        .enableDefaultTyping(
-                                BasicPolymorphicTypeValidator.builder()
-                                        .allowIfSubType(Object.class)
-                                        .build()
-                        )
-                        .build();
 
-        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(10))
-                .disableCachingNullValues()
-                .serializeKeysWith(
-                        RedisSerializationContext.SerializationPair.fromSerializer(
-                                new StringRedisSerializer()
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.activateDefaultTyping(
+                BasicPolymorphicTypeValidator.builder()
+                        .allowIfSubType(Object.class)
+                        .build(),
+                ObjectMapper.DefaultTyping.NON_FINAL
+        );
+
+        GenericJackson2JsonRedisSerializer serializer =
+                new GenericJackson2JsonRedisSerializer(objectMapper);
+
+        RedisCacheConfiguration config =
+                RedisCacheConfiguration.defaultCacheConfig()
+                        .entryTtl(Duration.ofMinutes(10))
+                        .disableCachingNullValues()
+                        .serializeKeysWith(
+                                RedisSerializationContext
+                                        .SerializationPair
+                                        .fromSerializer(new StringRedisSerializer())
                         )
-                )
-                .serializeValuesWith(
-                        RedisSerializationContext.SerializationPair.fromSerializer(serializer)
-                );
+                        .serializeValuesWith(
+                                RedisSerializationContext
+                                        .SerializationPair
+                                        .fromSerializer(serializer)
+                        );
 
         return builder -> builder
                 .cacheDefaults(config)
