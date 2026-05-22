ee3e1a7b100920e621cdb936fe8f3d063b50e2ab
김도현 <kimdoes2143@naver.com>
2026-05-23 05:11:03 +0900

    feat: 인증인가 작업 완료

--- src/test/java/com/conx/server/ConxServerApplicationTests.java
+++ src/test/java/com/conx/server/ConxServerApplicationTests.java
@@ -3,14 +3,11 @@ package com.conx.server;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @AutoConfigureMockMvc
