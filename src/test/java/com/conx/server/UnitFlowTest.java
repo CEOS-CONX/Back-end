7de084a0d257d66b14bc5dab5343165958d69d3e
김도현 <kimdoes2143@naver.com>
2026-07-19 16:50:21 +0900

    API 용 코드수정

--- src/test/java/com/conx/server/UnitFlowTest.java
+++ src/test/java/com/conx/server/UnitFlowTest.java
@@ -31,7 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
+@ActiveProfiles("local")
 public class UnitFlowTest {
 
     @Transactional
